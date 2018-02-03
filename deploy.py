456d0c1704d674b4065176362c0fb857348266fa
keremturgutlu <keremturgutlu@gmail.com>
2018-02-02 16:00:05 -0800

    deploy

---
+++ deploy.py
@@ -0,0 +1,37 @@
+'''
+Required Packages
+
+pip install paramiko
+
+
+
+'''
+
+import paramiko
+import time
+from user_definition import *
+
+#Connect to server
+print "Connecting to box"
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(hostname, username = username, key_filename = key_filename)
+
+if ssh == False:
+    print 'Connection Error'
+else: print 'Successful Connection'
+
+#Clone git repo to server
+
+"""
+git already installed in server
+
+
+"""
+
+ssh.exec_command('git clone https://github.com/KeremTurgutlu/sprint')
+
+
+
+
---
+++ user_definition.py
@@ -0,0 +1,3 @@
+hostname = '54.200.19.10'
+username = 'ec2-user'
+key_filename = '/home/kerem/.ssh/bowbow.pem'
