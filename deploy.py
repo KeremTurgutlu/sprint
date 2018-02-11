c96c3e61233ef0ae9dee136ab756786cfe1a30a2
t2liu <yliu266@usfca.edu>
2018-02-10 16:28:35 -0800

    edits5 to deploy.py

--- deploy.py
+++ deploy.py
@@ -22,14 +22,14 @@ def deploy(key_filename, hostname, prefix):
     git, crontab  already installed in server
     """
     # clone repo to home dir
-    ssh.exec_command('rm -rf sprint/; rm -r mycron; git clone https://github.com/KeremTurgutlu/sprint')
+    ssh.exec_command('rm -rf sprint/; rm -r mycron; crontab -r; git clone https://github.com/KeremTurgutlu/sprint')
     print 'Pull from github successfully!'
 
     # run process.py with crontab every 5 minutes
     ssh.exec_command('crontab -e mycron')
     # ssh.exec_command('crontab -e')
     # ssh.exec_command('echo "5 * * * * python ~/sprint/process.py {}" >> mycron'.format(prefix))
-    ssh.exec_command('echo "* * * * * python ~/sprint/process.py {}" >> mycron'.format(prefix))
+    ssh.exec_command('echo "5 * * * * python ~/sprint/process.py {}" >> mycron'.format(prefix))
 
     # ssh.exec_command('5 * * * * python ~/sprint/process.py {}' prefix)
 
