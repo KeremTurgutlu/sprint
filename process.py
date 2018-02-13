5f6673bba8d3ad03f6d596cc3dc83c03543a9477
t2liu <yliu266@usfca.edu>
2018-02-12 18:50:45 -0800

    edits

--- process.py
+++ process.py
@@ -26,13 +26,14 @@ print(good_files)
 for file in good_files:
     print 'reading {}...\n'.format(check_dir + file)
     with open(check_dir + file) as f:
-        try:
-            for line in f:
+        #try:
+        for line in f:
+            try:
                 d = json.loads(line)
                 name = d['name']
                 age = d['prop']['age']
                 name_age += name + '\t' + str(age) + '\n'
-        except:continue
+            except:continue
 
 # print 'Final Output\n'
 # print name_age
