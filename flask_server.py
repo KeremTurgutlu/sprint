c9d89b44708c894abcc8c94fcada9aefdf0656f2
DS-Lorean <ds-lorean@DS-Loreans-MacBook-Pro.local>
2018-02-26 20:07:16 -0800

    Out of function desperation

--- flask_server.py
+++ flask_server.py
@@ -14,26 +14,41 @@ def create_empty_text(path):
     with open(path, 'w') as f:
         pass
 
-def define_logger(path, when, interval):
-    """
-    Creates a logger object, which will take post requests and append them into txt file.
-    When the given interval is passed from the creation time of the most recent txt file, then a new
-    txt file will be created. This is more efficient then creating files every e.g. 5 minutes if there is no POST request.
-    But it will ensure that POST request within a file only includes the requests that are received within that interval.
+# def define_logger(path, when, interval):
+#     """
+#     Creates a logger object, which will take post requests and append them into txt file.
+#     When the given interval is passed from the creation time of the most recent txt file, then a new
+#     txt file will be created. This is more efficient then creating files every e.g. 5 minutes if there is no POST request.
+#     But it will ensure that POST request within a file only includes the requests that are received within that interval.
 
-    Inputs:
-        path (str): path for logging file
-        when (str): time component like 's' for seconds, 'm' for minutes
-        interval (int): interval to check and when to renew the file
-    Outputs:
-        logger (obj): logger object to use for appending files
-    """
-    logger = logging.getLogger("Rotating Log")
-    logger.setLevel(logging.INFO)
-    handler = TimedRotatingFileHandler(path, when=when, interval=interval)
-    logger.addHandler(handler)
+#     Inputs:
+#         path (str): path for logging file
+#         when (str): time component like 's' for seconds, 'm' for minutes
+#         interval (int): interval to check and when to renew the file
+#     Outputs:
+#         logger (obj): logger object to use for appending files
+#     """
+#     logger = logging.getLogger("Rotating Log")
+#     logger.setLevel(logging.INFO)
+#     handler = TimedRotatingFileHandler(path, when=when, interval=interval)
+#     logger.addHandler(handler)
 
-    return logger
+#     return logger
+
+# main functions
+prefix = sys.argv[1]
+#prefix = 'prefix'
+# create empty Raw.txt
+raw_path = '/srv/runme/' + prefix + '/Raw.txt'
+create_empty_text(raw_path)
+# create empty proc.txt
+proc_path = '/srv/runme/' + prefix + '/proc.txt'
+create_empty_text(proc_path)
+
+logger = logging.getLogger("Rotating Log")
+logger.setLevel(logging.INFO)
+handler = TimedRotatingFileHandler(raw_path, when='m', interval=2)
+logger.addHandler(handler)
 
 
 app = Flask(__name__)
@@ -67,15 +82,15 @@ def main():
 
 
 # main functions
-prefix = sys.argv[1]
-#prefix = 'prefix'
-# create empty Raw.txt
-raw_path = '/srv/runme/' + prefix + '/Raw.txt'
-create_empty_text(raw_path)
-# create empty proc.txt
-proc_path = '/srv/runme/' + prefix + '/proc.txt'
-create_empty_text(proc_path)
-# get logger
-logger = define_logger(raw_path, 'm', 2)
+# prefix = sys.argv[1]
+# #prefix = 'prefix'
+# # create empty Raw.txt
+# raw_path = '/srv/runme/' + prefix + '/Raw.txt'
+# create_empty_text(raw_path)
+# # create empty proc.txt
+# proc_path = '/srv/runme/' + prefix + '/proc.txt'
+# create_empty_text(proc_path)
+# # get logger
+# logger = define_logger(raw_path, 'm', 2)
 # run flask app
 app.run(host='0.0.0.0', port=8080, debug=True)
